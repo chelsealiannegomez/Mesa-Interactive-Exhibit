cbf0ac1e443b684e20a771bc13b6206e6f9792fe
chelsealiannegomez <ctgomez@asu.edu>
2024-10-05 16:01:00 -0700

    added 3d model book to roompage

--- threejs-react-app/src/App.js
+++ threejs-react-app/src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'; 
 import { Routes, Route } from 'react-router-dom';
 import StartingPage from './StartingPage';
-import Roompage from './roompage'
+import RoomPage from './RoomPage'
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<StartingPage />} />
-        <Route path="/room" element={<Roompage />} />
+        <Route path="/room" element={<RoomPage />} />
       </Routes>
     </>
   );
--- threejs-react-app/src/roompage.js
+++ threejs-react-app/src/roompage.js
@@ -1,9 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Sphere } from '@react-three/drei';
+import { OrbitControls, Sphere, useGLTF } from '@react-three/drei';
 import * as THREE from 'three';
 import { TextureLoader } from 'three';
-
 
 function Room() {
   const texture = new TextureLoader().load('/room/textures/theroomviewfinal.jpg');
@@ -15,12 +14,44 @@ function Room() {
   );
 }
 
-function RoomPage() {
+function BookModel() {
+  const { scene } = useGLTF('/paladins_book/scene.gltf');
   return (
-    <Canvas>
-      <OrbitControls enableZoom={false} />
-      <Room />
-    </Canvas>
+    <primitive 
+      object={scene} 
+      scale={[400, 400, 400]}
+      position={[300, -100, 130]}
+      rotation={[0, -Math.PI/2, 0]}
+    />);
+}
+
+function RoomPage() {
+  const [fade, setFade] = useState(true);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => setFade(false), 100);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <div className="relative w-full h-screen bg-white">
+      <div
+        className={`absolute inset-0 bg-white transition-opacity duration-5000 ease-out ${
+          fade ? 'opacity-100' : 'opacity-0'
+        } pointer-events-none z-20`}
+      />
+
+      <Canvas
+        className="w-full h-full bg-white"
+        camera={{ position: [-60, 20, 5] }}
+        style={{ backgroundColor: 'white' }}
+      >
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={4} /> 
+        <Room />
+        <BookModel />
+      </Canvas>
+    </div>
   );
 }
 
